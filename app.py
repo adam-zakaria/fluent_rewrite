b9adfbb620b85e6328560d95f8bce7a83a029c17
Adam Zakaria <adam.p.zakaria@gmail.com>
2025-06-18 13:36:33 -0400

    Update to use config

--- app.py
+++ app.py
@@ -6,26 +6,28 @@ import json
 from datetime import timedelta
 import model
 import helpers
-from config import Config
 
 app = Flask(__name__,
             static_url_path='',
             static_folder='static',
             template_folder='templates')
 # Session configuration
-app.secret_key = Config.SECRET_KEY
-app.config['DEBUG'] = Config.DEBUG
+#app.secret_key = 'dev'  # Change this to a real secret key in production
+#app.config['DEBUG'] = True
 app.config['SESSION_PERMANENT'] = True
 app.permanent_session_lifetime = timedelta(days=30)
 # Configure Flask-Session
-app.config['SESSION_TYPE'] = Config.SESSION_TYPE
-app.config['SESSION_FILE_DIR'] = Config.SESSION_FILE_DIR
+app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_FILE_DIR'] = 'flask_session'  # Directory to store session files
 Session(app)
 
 @app.context_processor
-def inject_api_host():
-    """Inject API_HOST into all templates"""
-    return dict(api_host=Config.get_api_host(request))
+def inject_env():
+    """Inject environment variables into all templates"""
+    return dict(api_host=os.getenv('API_HOST'),
+                debug=os.getenv('DEBUG'),
+                port=os.getenv('PORT'),
+                host=os.getenv('HOST'))
 
 @app.before_request
 def set_session_defaults():
@@ -95,4 +97,4 @@ def clear_data():
     return redirect(url_for('input'))
 
 if __name__ == '__main__':
-    app.run(debug=Config.DEBUG, host=Config.HOST, port=Config.PORT)
+    app.run(debug=True, host='0.0.0.0', port=3000)
